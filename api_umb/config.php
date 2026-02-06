443680d7fa847e95c294b4e901e4054761630346
xanzzz12 <122360582+xanzzz12@users.noreply.github.com>
2026-02-06 18:19:10 +0700

    Update config.php

--- api_umb/config.php
+++ api_umb/config.php
@@ -1,1 +1,15 @@
+<?php
+define('SECRET_KEY', 'RAHASIA_S_KOM_UMB_2026');
 
+// Ubah ke URL simulasi sesuai perintah dosen
+define('SOURCE_URL', 'http://localhost/sikawan.umb.ac.id/public/datapegawai/');
+
+function sendResponse($status, $message, $data = null) {
+    header("Content-Type: application/json");
+    echo json_encode([
+        "status" => $status,
+        "message" => $message,
+        "data" => $data
+    ]);
+    exit;
+}
