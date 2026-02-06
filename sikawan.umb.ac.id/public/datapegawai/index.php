b3cdc6d15f2ab7d5fed6836b9361d000fbae0b3f
xanzzz12 <122360582+xanzzz12@users.noreply.github.com>
2026-02-06 18:21:33 +0700

    Update index.php

--- sikawan.umb.ac.id/public/datapegawai/index.php
+++ sikawan.umb.ac.id/public/datapegawai/index.php
@@ -1,1 +1,19 @@
+<?php
+// index.php (Server Bayangan SIKAWAN)
+header("Content-Type: application/json");
 
+$dataDosen = [
+    "nidn" => "0123456789",
+    "np" => "1987654321",
+    "nama" => "Dr. Ahmad Fauzi, M.Kom",
+    "no_sertifikat" => "SER-2020-UMB-001",
+    "jabatan_fungsional" => "Lektor",
+    "tmt_jabatan" => "2020-01-01",
+    "pangkat_golongan" => "III/c",
+    "tmt_pangkat" => "2019-10-01",
+    "pts" => "Universitas Muhammadiyah Bengkulu",
+    "alamat" => "Bengkulu",
+    "no_hp" => "08123456789"
+];
+
+echo json_encode($dataDosen);
